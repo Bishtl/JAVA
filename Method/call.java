a536f22993a817a17e30e20ea2026bfee741d134
Rohit Bisht <102311349+Bishtl@users.noreply.github.com>
2022-09-21 11:45:02 +0530

    First-commit

---
+++ Method/call.java
@@ -0,0 +1,12 @@
+package Method;
+public class call{
+    static void Hello(){
+        System.out.println("hello");
+    }
+    
+    public static void main(String[] args) {
+        Hello();
+        Hello();
+        Hello();
+    }
+}
---
+++ Method/structure.java
@@ -0,0 +1,13 @@
+package Method;
+
+public class structure {
+    
+        static void Hello() {
+          System.out.println("HELLO ");
+        }
+      
+        public static void main(String[] args) {
+          Hello();
+        }
+      }
+
