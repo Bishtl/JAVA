6c3437631f520bf9237ff02d0ce52276d025eafc
Rohit Bisht <102311349+Bishtl@users.noreply.github.com>
2024-06-02 03:09:45 +0530

    first-commit

---
+++ Start-pattern/Number_changing_Pyramid_Pattern.java
@@ -0,0 +1,13 @@
+public class Number_changing_Pyramid_Pattern {
+    public static void main(String[] args) {
+        int k=0;
+        for(int i=1;i<=5;i++){
+            
+            for(int j=1;j<=i;j++){
+                k++;
+                System.out.print(k+" ");
+            }
+            System.out.println();
+        }
+    }
+}
